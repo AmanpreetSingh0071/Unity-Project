645a7c57e01736d7ae0793e088272b8879595063
AmanpreetSingh0071 <amanpreet.m.ahluwalia@gmail.com>
2025-03-18 10:07:55 +0000

    Unity_Project_environment_upload
    Complete zip file

---
+++ A-Life - CW2 - v2/Assets/RobotController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class RobotController : MonoBehaviour
+{
+    // naming constraints do not change
+    [SerializeField] private WheelCollider FLC;
+    [SerializeField] private WheelCollider FRC;
+    [SerializeField] private WheelCollider RLC;
+    [SerializeField] private WheelCollider RRC;
+
+    [SerializeField] private Transform FLT;
+    [SerializeField] private Transform FRT;
+    [SerializeField] private Transform RLT;
+    [SerializeField] private Transform RRT;
+
+    [SerializeField] private Transform FRS;
+    [SerializeField] private Transform L1S;
+    [SerializeField] private Transform L2S;
+    [SerializeField] private Transform L3S;
+    [SerializeField] private Transform R1S;
+    [SerializeField] private Transform R2S;
+    [SerializeField] private Transform R3S;
+    [SerializeField] private Transform ORS;
+
+
+    
+}
